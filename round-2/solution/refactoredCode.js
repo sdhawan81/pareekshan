99787d1556d32b6016bc43e1bc0b64025266a5af
Khushi Sarkari <ksarkari@ex2india.com>
2020-07-30 12:38:28 +0530

    code refactoring

---
+++ round-2/solution/refactoredCode.js
@@ -0,0 +1,26 @@
+class Customer {
+    constructor(nam, rentals) {
+        this.name = name;
+        this.rentals = rentals;
+    }
+    totalAmount() {
+        let totalAmount = 0;
+        let frequentRenterPoints = 0;
+        //calculate totalAmount
+
+    }
+    printResult(){
+        //generate output
+    }
+
+};
+var Rental = function () { 
+
+    var movies = {
+        F001: { title: 'Ran', code: 'regular' },
+        F002: { title: 'Trois Couleurs: Bleu', code: 'regular' },
+        F003: { title: 'Cars 2', code: 'childrens' },
+        F004: { title: 'Latest Hit Release', code: 'new' },
+    };
+    return movies;
+}
